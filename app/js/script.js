7a1f3c4d1df6808683e958d7a6d03e1245d59abd
Alexey Zavalin <lehazavalin95@gmail.com>
2016-07-18 20:41:30 +1000

    Мелочи остались

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {	
+$(document).ready(function() {
 	$('.check__form label').on ('click', function() {
 		$(this).parents('a').toggleClass('checked');
+		var i = 0;
+		$('.course a').each(function() {
+			if ($(this).hasClass('checked')) {
+				i++;
+			}
+		});
+		if (i == 6) {
+			$('.course a').parents('.course').css('visibility','hidden');
+			$('.course__title').css('visibility','hidden');
+			$('.complete').slideDown(1300);
+		}			
 	});
 });
