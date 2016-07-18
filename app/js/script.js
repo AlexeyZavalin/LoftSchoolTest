7b62fd4745ad00ac6a829aa805990534c9f5b30b
Alexey Zavalin <lehazavalin95@gmail.com>
2016-07-18 21:21:34 +1000

    complete

--- app/js/script.js
+++ app/js/script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$('.check__form label').on ('click', function() {
 		$(this).parents('a').toggleClass('checked');
 		var i = 0;
+		$(this).parents('.course').fadeTo(600, 0).css('visibility','hidden');
 		$('.course a').each(function() {
 			if ($(this).hasClass('checked')) {
 				i++;
 			}
 		});
-		if (i == 6) {
-			$('.course a').parents('.course').css('visibility','hidden');
+		if (i == 6) {			
 			$('.course__title').css('visibility','hidden');
 			$('.complete').slideDown(1300);
 		}			
